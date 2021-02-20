1c46a8f513805b28d2867532761f00262b1fde31
MONK <zhangruisheng@outlook.com>
2021-02-20 17:42:52 +0800

    更新

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-(function ($) {
+$(function () {
     "use strict";
 
     //显示博客文章特征图像
@@ -15,25 +15,22 @@
             $(this).addClass('active-post');
         });
     }
-})(jQuery);
+});
 
-(function () {
+$(function () {
     var light = "#fff";
     var dark = "#000";
     var oadark = "#1A161C"
     var oalight = "#fafafa"
     if (new Date().getHours() > 13 || new Date().getHours() < 8) {
-
         document.documentElement.style.setProperty('--color-ff', dark);
         document.documentElement.style.setProperty('--color-00', light);
         document.documentElement.style.setProperty('--color-oa', oadark);
-
         console.log('夜间模式开启');
     } else {
-
         document.documentElement.style.setProperty('--color-ff', light);
         document.documentElement.style.setProperty('--color-00', dark);
         document.documentElement.style.setProperty('--color-oa', oalight);
         console.log('夜间模式关闭');
     }
-})();
+});
