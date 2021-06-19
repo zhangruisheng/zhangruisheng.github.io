9bc4575ba4579e8333da20679a9fd2f8f16b65a8
MONK.REN <zhangruisheng@outlook.com>
2021-06-19 17:06:41 +0800

    xxx

--- article/js/art.js
+++ article/js/art.js
@@ -17,30 +17,20 @@
 
 
 
-$(function () {
-    var h1 = 400;
-    var h2 = 480;
-    var ss = $(document).scrollTop();
-    $(window).scroll(function () {
-        var s = $(document).scrollTop();
-
-        if (s < h1) {
-            $('.yapiskan').removeClass('yya');
-        }
-        if (s > h1) {
-            $('.yapiskan').addClass('yya');
-        }
-        if (s > h2) {
-            $('.yapiskan').addClass('gizle');
-            if (s > ss) {
-                $('.yapiskan').removeClass('sabit');
-            } else {
-                $('.yapiskan').addClass('sabit');
-            }
-            ss = s;
-        }
+//联系我展开与关闭
+var open1 = document.getElementById("open-button");
+var close1 = document.getElementById("close-button");
 
 
-    });
+close1.onclick = function () {
+    "use strict";
+    document.getElementById("menu").setAttribute("class", "menu");
+    document.getElementById("zzhao").setAttribute("class", "zzzhao");
+};
 
-});
+open1.onclick = function () {
+    "use strict";
+    document.getElementById("menu").setAttribute("class", "menu-open");
+    document.getElementById("zzhao").setAttribute("class", "zzhao");
+
+};
