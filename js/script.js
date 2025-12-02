d0162bca0d79d14d0c4a81858c008bc143fa2763
MONK.REN <zhangruisheng@outlook.com>
2025-12-02 20:32:30 +0800

    xx

--- js/script.js
+++ js/script.js
@@ -7,24 +7,31 @@ $.each( instance, function(key, value) {
       nextArrow = arrows.filter('.arrow-next'),
       box = $(instance[key]).find(".dazzle-imgs"), 
       x = 0,
-      mx = 0,
-      maxScrollWidth = box[0].scrollWidth - (box[0].clientWidth / 2) - (box.width() / 2);
+      mx = 0;
+  
+  if (!box.length || !box[0]) {
+    return; // skip this instance if required DOM is missing
+  }
 
-  $(arrows).on('click', function() {
-      
-    if ($(this).hasClass("arrow-next")) {
-      x = ((box.width() / 2)) + box.scrollLeft() - 10;
-      box.animate({
-        scrollLeft: x,
-      })
-    } else {
-      x = ((box.width() / 2)) - box.scrollLeft() -10;
-      box.animate({
-        scrollLeft: -x,
-      })
-    }
-      
-  });
+  var maxScrollWidth = box[0].scrollWidth - (box[0].clientWidth / 2) - (box.width() / 2);
+
+  if (arrows && arrows.length) {
+    $(arrows).on('click', function() {
+        
+      if ($(this).hasClass("arrow-next")) {
+        x = ((box.width() / 2)) + box.scrollLeft() - 10;
+        box.animate({
+          scrollLeft: x,
+        })
+      } else {
+        x = ((box.width() / 2)) - box.scrollLeft() -10;
+        box.animate({
+          scrollLeft: -x,
+        })
+      }
+        
+    });
+  }
     
   $(box).on({
     mousemove: function(e) {
@@ -58,4 +65,26 @@ $.each( instance, function(key, value) {
       }
   }
   
+});
+
+//菜单项点击切换
+$(document).ready(function() {
+    $('#open-button').click(function() {
+        $('nav').toggleClass('menu-open');
+    });
+    $('#close-button').click(function() {
+        $('nav').removeClass('menu-open');
+    });
+    $('.zzhao').click(function() {
+        $('nav').removeClass('menu-open');
+    });
+    
+    // 移动端子菜单切换
+    $('.menu li').has('.submenu').click(function(e) {
+        // 阻止事件冒泡，避免触发菜单项的链接跳转
+        if ($(e.target).closest('a').length === 0 || $(e.target).closest('a').parent().hasClass('submenu')) {
+            e.preventDefault();
+            $(this).toggleClass('active');
+        }
+    });
 });
